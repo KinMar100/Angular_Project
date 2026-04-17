3b4df574ca2bc9b3ab6f5ed7ed06a86948e5b42a
KinMar100 <markowska.kinga13@gmail.com>
2026-04-17 21:37:43 +0200

    commit (04-17)

--- src/main.ts
+++ src/main.ts
@@ -36,6 +36,7 @@ export class DataService
     <a target="_blank" href="https://angular.dev/overview">
       Learn more about Angular
     </a>
+    <p></p>
     <button (click)="counter.set(counter() - 1)">--</button>
     <span> Counter: {{ counter() }} </span>
     <button (click)="counter.set(counter() + 1)">++</button>
@@ -91,20 +92,44 @@ export class App {
   ifFail: boolean = false;
   ifSuccess: boolean = false;
 
+  workers = [
+    {name: "Kate", result: 85, confirm: true},
+    {name: "Tom", result: 40, confirm: false},
+    {name: "Mark", result: 92, confirm: true},
+    {name: "Sofie", result: 55, confirm: false}
+  ];
+
+  projects = [
+    {name: "Online Shop", status: "Success", budget: 10000},
+    {name: "Corporate Blog", status: "In Progress", budget: 2000},
+    {name: "Client Portal", status: "Success", budget: 15000},
+    {name: "Mobile App", status: "Fail", budget: 8000},
+  ];
+
+  confirmed: any[] = [];
+  nameConfirmed: string[] = [];
+
   addTask() {
-    if (this.newTask.trim() !== '') {
+    if (this.newTask.trim() !== '') 
+    {
       this.listOfTasks.push(this.newTask);
 
       this.newTask = '';
       this.error = false;
-    } else {
+    }
+    else
+    {
       this.error = true;
     }
   }
 
   constructor(private dataService: DataService){}
 
-  listExample1 = ["Anne", "Bart", "Chester", "David"];
+  listExample = ["Anne", "Bart", "Chester", "David"];
+
+  sum: number = 0;
+
+  sumProfit: number = 0;
 
   import()
   {
@@ -121,6 +146,31 @@ export class App {
       this.ifSuccess = false;
     }
   }
+
+  processedWorkers()
+  {
+    this.confirmed = this.workers.filter(worker => worker.confirm === true);
+
+    this.nameConfirmed = this.workers.map(worker => worker.name);
+  }
+
+  calculateSumOfPoints()
+  {
+    this.sum = this.workers.reduce((sum, worker) =>
+    {return sum + worker.result}, 0);
+  }
+
+  calculateBudgetSuccess()
+  {
+    this.sumProfit = this.projects
+    .filter(project => project.status === "Success")
+    .map(project => project.budget)
+    .reduce((sum, element) => sum + element, 0);
+
+    return this.sumProfit;
+    
+  }
+
 }
 
 bootstrapApplication(App);
